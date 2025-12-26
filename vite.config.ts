835a728430dfcc21f45df15948b09f2a91425600
renjp <r979668507@163.com>
2025-12-26 15:07:29 +0800

    fix: pdf 问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,36 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
-export default defineConfig(({ mode }) => {
+
+export default defineConfig(({mode}) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: 'dev-toolbox-aggregator',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        base: 'dev-toolbox-aggregator',
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [
+            react(),
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: 'node_modules/pdfjs-dist/cmaps/', // 注意末尾的斜杠
+                        dest: '' // 默认会复制到 dist/cmaps
+                    }
+                ]
+            })
+        ],
+        define: {
+            'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+            'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
         }
-      }
     };
 });
