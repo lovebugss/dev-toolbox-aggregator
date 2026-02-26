6891821a703e6bc1bbad0749c7365c0fe6bae6b3
renjp <r979668507@163.com>
2026-02-26 15:12:09 +0800

    feat: 增加manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import {viteStaticCopy} from 'vite-plugin-static-copy'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 
 export default defineConfig(({mode}) => {
@@ -13,6 +15,12 @@ export default defineConfig(({mode}) => {
             host: '0.0.0.0',
         },
         plugins: [
+            [VitePWA({
+                registerType: 'autoUpdate',
+                workbox: {
+                    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+                }
+            })],
             react(),
             viteStaticCopy({
                 targets: [
@@ -32,8 +40,9 @@ export default defineConfig(({mode}) => {
                 '@': path.resolve(__dirname, '.'),
             }
         },
-        build: {
-            manifest: true
-        }
+        // build: {
+        //     manifest: true
+        // },
+
     };
 });
